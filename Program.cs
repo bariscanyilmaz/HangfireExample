54688cae2b991b217bca339a1508764fb34fe662
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-01-09 04:22:55 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+
+namespace HangfireExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            GlobalConfiguration.Configuration
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSqlServerStorage(@"Server=.\SQLEXPRESS;Database=Hangfire.Sample; Integrated Security=True;");
+
+            
+
+            //Fire-and-Forget
+            BackgroundJob.Enqueue(()=>Console.WriteLine("Fire-and-Forget "));
+
+            //Recurring
+            RecurringJob.AddOrUpdate(()=>Console.WriteLine("Recurring "),Cron.Daily);
+
+            //Delayed Jobs
+            var jobId= BackgroundJob.Schedule(()=>Console.WriteLine("10 seconds Delayed"),TimeSpan.FromSeconds(10));
+
+            //Continuations
+            BackgroundJob.ContinueJobWith(jobId,()=>Console.WriteLine("Continuations"));
+
+             using (var server = new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+            
+        }
+
+
+    }
+}
